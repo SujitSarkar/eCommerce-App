0c20e65663a09a7aa1a02547b3af3c176c1f85d9
SujitSarkar <snigdho532@gmail.com>
2020-08-03 07:29:59 +0600

    Kinbo Ami commited

---
+++ app/src/main/java/sarkar/kinboami/SearchProducts.java
@@ -0,0 +1,14 @@
+package sarkar.kinboami;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchProducts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_products);
+    }
+}
